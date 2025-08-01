ceee5bf337e4403ab3dc4d61e1b052e3c7de2a3e
Michel Arruda <michel.diasdearruda@gmail.com>
2025-08-01 18:43:01 -0300

    Adicionar CORS

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from routers import upload, rag, classify
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="CGU Data Scientist Challenge API", 
               docs_url="/docs", 
               redoc_url="/redoc")
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(upload.router, prefix="/upload", tags=["Upload"])
 app.include_router(rag.router, prefix="/rag", tags=["RAG"])
